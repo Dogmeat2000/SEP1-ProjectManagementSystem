d6abb38ecab7113c49e5defc54cdc5640441b87c
Alperen941 <Alperenozer03@gmail.com>
2023-11-29 23:37:32 +0100

    Finished methods to PromotionalInformation

--- src/main/java/sep1a233group/bobsprojectmanagementsystem/PromotionalInformation.java
+++ src/main/java/sep1a233group/bobsprojectmanagementsystem/PromotionalInformation.java
@@ -1,6 +1,72 @@
 package sep1a233group.bobsprojectmanagementsystem;
 
+import java.util.ArrayList;
+
 public class PromotionalInformation
 {
+  private String photoURL, projectDescription, projectName;
+  private ArrayList<String>projectManagerComments;
+
+  public PromotionalInformation(String projectName)
+  {
+    this.photoURL = null;
+    this.projectDescription = null;
+    this.projectName = projectName;
+    this.projectManagerComments = new ArrayList<>();
+  }
+
+  public String getPhotoURL()
+  {
+    return photoURL;
+  }
+
+  public void addPhotoURL(String photoURL)
+  {
+    this.photoURL = photoURL;
+  }
+
+  public void removePhotoURL()
+  {
+    this.photoURL = null;
+  }
+
+  public String getProjectDescription()
+  {
+    return projectDescription;
+  }
+
+  public void setProjectDescription(String projectDescription)
+  {
+    this.projectDescription = projectDescription;
+  }
+
+  public String getProjectName()
+  {
+    return projectName;
+  }
+
+  public void setProjectName(String projectName)
+  {
+    this.projectName = projectName;
+  }
+
+  public String getProjectManagerComments()
+  {
+    for (String managerComments:projectManagerComments)
+    {
+      return managerComments;
+    }
+    return null;
+  }
+
+  public void addProjectManagerComment(String comment)
+  {
+    projectManagerComments.add(comment);
+  }
+
+  public void deleteProjectManagerCommentLine(int index)
+  {
+    projectManagerComments.remove(index);
+  }
 
 }
