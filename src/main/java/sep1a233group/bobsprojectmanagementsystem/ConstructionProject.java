5f52151d73e8e5a846be39f8ddcb4967dafeacb5
Alperen941 <Alperenozer03@gmail.com>
2023-11-30 20:27:39 +0100

    made attributes and methods to ConstructionProject

--- src/main/java/sep1a233group/bobsprojectmanagementsystem/ConstructionProject.java
+++ src/main/java/sep1a233group/bobsprojectmanagementsystem/ConstructionProject.java
@@ -2,17 +2,57 @@ package sep1a233group.bobsprojectmanagementsystem;
 
 public abstract class ConstructionProject
 {
+  private Address projectAddress; //What is the address of the Project?
+  private Customer customer; // who is the project for?
+  private PromotionalInformation projectInformation; // What information do we have about the project?
+  private HumanRessources humanRessources; // how much manhours is used for the project(current and expectied to finish)?
+  private Finances finances; //how much is spent on the project?. what is the budget for the project?
+  private ProgressReport progressReport; // how is the project going?
+  private boolean isProjectFinished; //is the project finished?
+  private boolean projectConfidentiality; // is the project confidential?
   private MyDate projectStartDate; //When was the project created/initiated?
   private MyDate projectEndDate; //When was the project finished (or expected to finish?)
 
   //TODO: IMPLEMENT CLASS
 
-  public ConstructionProject()
+  public ConstructionProject(PromotionalInformation projectInformation, Address projectAddress, Customer customer, boolean projectConfidentiality, MyDate projectStartDate, MyDate projectEndDate, HumanRessources humanRessources, Finances finances,ProgressReport progressReport)
   {
     //TODO: Implement... ALSO CONVERT THESE TEMPORARY DATA SETS TO SETTERS AND GETTERS!
-    this.projectStartDate = new MyDate(1,1,2000);
-    this.projectEndDate = new MyDate(1,1,2000);
+    this.projectInformation = projectInformation;
+    this.projectAddress = projectAddress;
+    this.customer = customer;
+    this.projectConfidentiality = projectConfidentiality;
+    this.projectStartDate = projectStartDate;
+    this.projectEndDate = projectEndDate;
+    this.humanRessources = humanRessources;
+    this.finances = finances;
+    this.progressReport = progressReport;
+    isProjectFinished = false;
   }
+
+  public Address getProjectAddress() { return projectAddress; }
+
+  public Customer getCustomer() { return customer; }
+
+  public PromotionalInformation getProjectInformation() { return projectInformation; }
+
+  public HumanRessources getHumanRessources() { return humanRessources; }
+
+  public Finances getFinances() { return finances; }
+
+  public ProgressReport getProgressReport() { return progressReport; }
+
+  public boolean isProjectFinished() { return isProjectFinished; }
+
+  public void setProjectFinished(boolean projectFinished) { isProjectFinished = projectFinished; }
+
+  public boolean isProjectConfidential() { return projectConfidentiality; }
+
+  public void setProjectConfidentiality(boolean projectConfidentiality) { this.projectConfidentiality = projectConfidentiality; }
+
+  public MyDate getProjectStartDate() { return projectStartDate; }
+
+  public MyDate getProjectEndDate() { return projectEndDate;}
 
   /** This method is compares the given ConstructionProject object with the current ConstructionProject object and returns true if they are identical.
    * It is for instance used to validate attempts of adding projects to the system in order to avoid adding duplicates.
